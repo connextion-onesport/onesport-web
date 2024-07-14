8a635636b5f200fb3b8edec829a5277c094308f7
dimmasyusuf <dimasyusufqurohman@gmail.com>
2024-07-14 15:52:58 +0700

    chore: install and configure shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center">
-      <button className="rounded-md bg-blue-500 px-4 py-2 text-white">
-        Click me
-      </button>
+      <Button className="bg-primary text-white">OneSport</Button>
     </main>
   );
 }
