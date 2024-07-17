b09bd3b0448a9e922889c1f83a4abe30f9c3e864
dimmasyusuf <dimasyusufqurohman@gmail.com>
2024-07-17 10:39:39 +0700

    style(CTA): create cta ui

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -1,7 +1,19 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
 export default function CTA() {
   return (
-    <div className="flex w-full items-center justify-center rounded-lg border border-gray-300 p-8">
-      <p className="font-bold">CTA</p>
-    </div>
+    <section className="flex w-full flex-col items-center justify-center gap-4 p-8">
+      <div className="flex flex-col items-center justify-center">
+        <h3 className="text-2xl font-bold">Latest Updates</h3>
+        <p className="text-sm">Stay connected with us on Instagram</p>
+      </div>
+
+      <Button size="lg" asChild>
+        <Link href="https://instagram.com/" target="_blank">
+          Follow Us
+        </Link>
+      </Button>
+    </section>
   );
 }
