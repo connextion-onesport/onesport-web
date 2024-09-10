404a65763365c5e02d3c8d762ee3de1e0615af65
dimmasyusuf <dimasyusufqurohman@gmail.com>
2024-09-10 22:45:07 +0700

    fix: move suspense at fields page for using useSearchParams

--- app/(root)/fields/page.tsx
+++ app/(root)/fields/page.tsx
@@ -59,24 +59,24 @@ export default function FieldsPage() {
   };
 
   return (
-    <Suspense>
-      <div className="mx-auto flex w-full max-w-screen-2xl flex-col">
-        <section className="flex flex-col gap-4 p-4 md:p-8">
-          <Searchbar onSearch={handleSearch} />
-          <FilterButtonList
-            onOrder={handleOrder}
-            onRating={handleRating}
-            onCategory={handleCategory}
-          />
-        </section>
+    <div className="mx-auto flex w-full max-w-screen-2xl flex-col">
+      <section className="flex flex-col gap-4 p-4 md:p-8">
+        <Searchbar onSearch={handleSearch} />
+        <FilterButtonList
+          onOrder={handleOrder}
+          onRating={handleRating}
+          onCategory={handleCategory}
+        />
+      </section>
 
-        <NavbarBottom />
+      <NavbarBottom />
 
+      <Suspense>
         <FieldList
           title="Rekomendasi Tempat Olahraga"
           description="Daftar tempat olahraga yang ada di OneSport"
         />
-      </div>
-    </Suspense>
+      </Suspense>
+    </div>
   );
 }
